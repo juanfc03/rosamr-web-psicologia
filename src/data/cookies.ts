c9828e89f6d4eb92af7178a65d6f05c4b4d18379
Juan Fernández Ceacero <juanf.ceacero@gmail.com>
2026-05-15 15:56:07 +0200

    feat: añadir componentes de páginas legales (Cookies, LegalAdvice, Privacy) y extraer datos de cookies a archivo independiente

---
+++ src/data/cookies.ts
@@ -0,0 +1,130 @@
+type TipoCookie = {
+  nombre: string;
+  duracion: string;
+  proposito: string;
+  intrusion: string;
+};
+
+export const cookies = [
+  {
+    nombre: '__Secure-1PAPISID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para fines de segmentación y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-1PSID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para fines de segmentación y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-1PSIDCC',
+    duracion: '1 año',
+    proposito: 'Seguridad y protección de la cuenta del usuario (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-1PSIDTS',
+    duracion: '1 año',
+    proposito: 'Seguridad y protección de la cuenta del usuario (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-3PAPISID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para fines de segmentación y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-3PSID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para fines de segmentación y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-3PSIDCC',
+    duracion: '1 año',
+    proposito: 'Seguridad y protección de la cuenta del usuario (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-3PSIDTS',
+    duracion: '1 año',
+    proposito: 'Seguridad y protección de la cuenta del usuario (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: '__Secure-ENID',
+    duracion: '13 meses',
+    proposito: 'Preferencias del usuario y seguridad al usar Google Maps',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'AEC',
+    duracion: '6 meses',
+    proposito: 'Cookies de seguridad de Google Maps para evitar abusos',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'APISID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para almacenar preferencias y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'HSID',
+    duracion: '13 meses',
+    proposito: 'Seguridad en Google Maps: contiene registros encriptados',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'NID',
+    duracion: '6 meses',
+    proposito: 'Visualización de publicidad y preferencias en Google Maps',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'SAPISID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para almacenar preferencias y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'SEARCH_SAMESITE',
+    duracion: '6 meses',
+    proposito: 'Correcto funcionamiento de envío de datos en Google Maps',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'SID',
+    duracion: '13 meses',
+    proposito: 'Seguridad en Google Maps: contiene registros encriptados',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'SIDCC',
+    duracion: '1 año',
+    proposito: 'Seguridad y protección de la cuenta del usuario (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'SSID',
+    duracion: '13 meses',
+    proposito:
+      'Utilizada por Google Maps para almacenar preferencias y publicidad',
+    intrusion: 'Nivel 3',
+  },
+  {
+    nombre: 'UULE',
+    duracion: '~5 horas',
+    proposito: 'Personaliza resultados locales (Google Maps)',
+    intrusion: 'Nivel 3',
+  },
+] as const satisfies readonly TipoCookie[];
